d360a563fce7d017c0f047708f7d9faf16dfe3ac
pedr0206 <ped@umbraco.dk>
2018-09-12 16:28:17 +0200

    Add project files.

---
+++ BritishAutomobiles/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BritishAutomobiles
+{
+    public class Car
+    {
+        public string Manufacture { get; set; }
+        public string Model { get; set; }
+        public double EngineSize { get; set; }
+        public string FuelType { get; set; }
+        public string ValveTrainConfig { get; set; }
+    }
+}
---
+++ BritishAutomobiles/Segment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BritishAutomobiles
+{
+    public class Segment
+    {
+        public enum SegmentEnum
+        {
+            Segment1,
+            Segment2,
+            Segment3,
+            Segment4,
+            Segment5
+        }
+    }
+}
